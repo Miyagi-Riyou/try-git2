1a83d5b058e8b4dcb7e02131b368cbee7791e559
miyagiriyou <miyagi-riyou@occ.co.jp>
2020-05-12 16:04:11 +0900

    イニシャルコミット

---
+++ try-github/try-github/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace try_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Hello GitHub!");
+            
+        }
+    }
+}
